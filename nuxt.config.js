8b0c88e2776518ea2218360db0367975985ebda8
Denny Jose Vigil Majano <denny.majano@outlook.com>
2021-07-21 22:32:25 -0600

    correccion preview 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,22 +37,9 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
     'bootstrap-vue/nuxt'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
